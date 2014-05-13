593a6271d41592861f05a95fdc729ee384bd70b7
Ciro Costa <ciro.costa@usp.br>
2014-05-13 15:17:17 -0300

    updates main

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
 var _ = require('underscore');
 
 function isSimilarTo (a, b) {
-    var keysA, keysB;
+  var keysA, keysB;
 
-    console.log('');
-    console.log(a,b);
+  if (!(_.isObject(a) && _.isObject(b)))
+    return true;
 
-    if (!(_.isObject(a) && _.isObject(b))) {
-        return true;
-    }
 
-    if (_.isEqual(a, b)) return true;
+  if (_.isEqual(a, b)) return true;
 
-    keysA = Object.keys(a);
-    keysB = Object.keys(b);
+  keysA = Object.keys(a);
+  keysB = Object.keys(b);
 
-    if (!_.isEqual(keysA, keysB)) return false;
+  if (!_.isEqual(keysA, keysB)) return false;
 
-    for (var key in keysA) {
-        return isSimilarTo(a[keysA[key]], b[keysA[key]]);
-    }
+  for (var key in keysA)
+    return isSimilarTo(a[keysA[key]], b[keysA[key]]);
 }
 
 module.exports = isSimilarTo;
