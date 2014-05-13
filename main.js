03394247405307908cd3836d7a580a6c15f77389
Ciro Costa <ciro.costa@usp.br>
2014-05-13 15:09:06 -0300

    now working

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 var _ = require('underscore');
 
 function isSimilarTo (a, b) {
+    var keysA, keysB;
 
-    if (a === b) return true;
+    console.log('');
+    console.log(a,b);
 
-    var keysA = Object.keys(a);
-    var keysB = Object.keys(b);
+    if (!(_.isObject(a) && _.isObject(b))) {
+        return true;
+    }
+
+    if (_.isEqual(a, b)) return true;
+
+    keysA = Object.keys(a);
+    keysB = Object.keys(b);
 
     if (!_.isEqual(keysA, keysB)) return false;
 
     for (var key in keysA) {
-        isSimilarTo(a[key], b[key]);
+        return isSimilarTo(a[keysA[key]], b[keysA[key]]);
     }
-
-    return true;
 }
 
 module.exports = isSimilarTo;
--- test.js
+++ test.js
@@ -3,51 +3,51 @@ var isSimilarTo = require('./main.js');
 var _ = require('underscore');
 
 
-describe('SimilarTo', function () {
+describe('with SimilarTo,', function () {
 
-    it('should be sane', function () {
-        assert(!!isSimilarTo);
+  it('should be sane', function () {
+    assert(!!isSimilarTo);
+  });
+
+  describe('empty objects', function () {
+    it('should be equal if both undefined', function () {
+      assert(isSimilarTo(undefined, undefined));
     });
 
-    describe('empty objects', function () {
-        it('should be equal if both undefined', function () {
-            assert(isSimilarTo(undefined, undefined));
-        });
+    it('should return true to identical objs', function () {
+      var a = {a: 'a'};
+      var b = {a: 'a'};
 
-        it('should return true to identical objs', function () {
-            var a = {a: 'a'};
-            var b = {a: 'a'};
-
-            assert(isSimilarTo(a,b));
-        });
-
-        it('should return true to similar objects', function () {
-            var a = {a: 'a'};
-            var b = {a: 'b'};
-
-            assert(isSimilarTo(a,b));
-        });
-
-        it('should return false to non-similar objects', function () {
-            var a = {a: 'a'};
-            var b = {b: 'b'};
-
-            assert(!isSimilarTo(a,b));
-        });
-
-        it('should return true to 2layer similar objects', function () {
-            var a = {a: {b: 'b'}};
-            var b = {a: {b: 'c'}};
-
-            assert(isSimilarTo(a,b));
-        });
-
-        it('should return false to non-similar 2layer objects', function () {
-            var a = {a: {b: 'a'}};
-            var b = {a: {c: 'c'}};
-
-            assert(!isSimilarTo(a,b));
-        });
+      assert(isSimilarTo(a,b));
     });
+
+    it('should return true to similar objects', function () {
+      var a = {a: 'a'};
+      var b = {a: 'b'};
+
+      assert(isSimilarTo(a,b));
+    });
+
+    it('should return false to non-similar objects', function () {
+      var a = {a: 'a'};
+      var b = {b: 'b'};
+
+      assert(!isSimilarTo(a,b));
+    });
+
+    it('should return true to 2layer similar objects', function () {
+      var a = {a: {b: 'b'}};
+      var b = {a: {b: 'c'}};
+
+      assert(isSimilarTo(a,b));
+    });
+
+    it('should return false to non-similar 2layer objects', function () {
+      var a = {a: {b: 'a'}};
+      var b = {a: {c: 'c'}};
+
+      assert(!isSimilarTo(a,b));
+    });
+  });
 
 });
