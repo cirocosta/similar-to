b6e272770eaa7d47d1566e8b5b352cc0b19887eb
Ciro S. Costa <ciro.costa@usp.br>
2014-09-03 01:06:58 -0300

    adds travis, editorconfig, and reestructures the interface

--- main.js
+++ main.js
@@ -1,33 +1,39 @@
-function fullySimilar (a, b) {
-  'use strict';
-  var _ = require('underscore');
-  var keysA, keysB;
+'use strict';
 
-  if (!(_.isObject(a) && _.isObject(b)))
+var _ = require('underscore');
+
+/**
+ * Verifies if an object is similar to another,
+ * i.e, it contains the fields that the other
+ * contains (or at least partially, if specified
+ * in the 3rd param)
+ * @param {Object} a the test object
+ * @param {Object} b the example object
+ * @param {Boolean} partial if the check should
+ * be for partial similarity
+ * @return {Boolean}   if it is partially
+ * similar or not
+ */
+function similarTo (a, b, partial) {
+  if (!(_.isObject(a) && _.isObject(b)) || _.isEqual(a, b))
     return true;
 
-  if (_.isEqual(a, b)) return true;
+  var keysA, keysB;
 
   keysA = Object.keys(a);
   keysB = Object.keys(b);
 
-  if (!_.isEqual(keysA, keysB)) return false;
+  if (!partial)
+    if (!_.isEqual(keysA, keysB))
+      return false;
+  else
+    if (_.intersection(keysA, keysB) >= keysB.length)
+      return false;
 
   for (var key in keysA)
-    return fullySimilar(a[keysA[key]], b[keysA[key]]);
-}
-
-// TODO
-function partiallySimilar (a, b) {
-  'use strict';
-  var _ = require('underscore');
-
-  if (_.isEqual(a, b)) return true;
+    return similarTo(a[keysA[key]], b[keysA[key]], partial);
 
   return false;
 }
 
-module.exports = {
-  fullySimilar: fullySimilar,
-  partiallySimilar: partiallySimilar
-};
+module.exports = similarTo;
--- test.js
+++ test.js
@@ -2,20 +2,16 @@ var assert = require('assert');
 var similarTo = require('./main.js');
 var _ = require('underscore');
 
-var partiallySimilar = similarTo.partiallySimilar;
-var fullySimilar = similarTo.fullySimilar;
+describe('SimilarTo,', function () {
 
-
-describe('from SimilarTo,', function () {
-
-  describe('fullySimilar,', function () {
+  describe('similarTo,', function () {
     it('should be sane', function () {
-      assert(!!fullySimilar);
+      assert(!!similarTo);
     });
 
     describe('empty objects', function () {
       it('should be equal if both undefined', function () {
-        assert(fullySimilar(undefined, undefined));
+        assert(similarTo(undefined, undefined));
       });
     });
 
@@ -24,66 +20,60 @@ describe('from SimilarTo,', function () {
         var a = {a: 'a'};
         var b = {a: 'a'};
 
-        assert(fullySimilar(a, b));
+        assert(similarTo(a, b));
       });
 
       it('should return true to similar 1level objects', function () {
         var a = {a: 'a'};
         var b = {a: 'b'};
 
-        assert(fullySimilar(a, b));
+        assert(similarTo(a, b));
       });
 
       it('should return false to non-similar objects', function () {
         var a = {a: 'a'};
         var b = {b: 'b'};
 
-        assert(!fullySimilar(a, b));
+        assert(!similarTo(a, b));
       });
 
       it('should return true to 2layer similar objects', function () {
         var a = {a: {b: 'b'}};
         var b = {a: {b: 'c'}};
 
-        assert(fullySimilar(a, b));
+        assert(similarTo(a, b));
       });
 
       it('should return false to non-similar 2layer objects', function () {
         var a = {a: {b: 'a'}};
         var b = {a: {c: 'c'}};
 
-        assert(!fullySimilar(a, b));
+        assert(!similarTo(a, b));
       });
 
       it('should return false if one of them doesnt has all fields', function () {
         var a = {a: {b: 'a'}};
         var b = {a: {}};
 
-        assert(!fullySimilar(a, b));
+        assert(!similarTo(a, b));
       });
     });
   });
 
-  describe('PartiallySimilar,', function () {
-    it('should be sane', function () {
-      assert(!!partiallySimilar);
-    });
-
+  describe('PartialSimilarity,', function () {
     it('should return true if A === B', function () {
       var a = {a: '', b: ''};
       var b = {a: '', b: ''};
 
-      assert(partiallySimilar(b, a));
+      assert(similarTo(b, a, true));
     });
 
     it('should return true if A contains B', function () {
       var a = {a: '', b: ''};
       var b = {a: ''};
 
-      assert(partiallySimilar(b, a));
+      assert(similarTo(b, a, true));
     });
-
-
   });
 
 });
